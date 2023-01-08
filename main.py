58ae13c1bc9bcbe00ed55319088d1e5e24a0b513
deepak2431 <deepakdk2431@gmail.com>
2023-01-08 22:42:24 +0530

    added router

--- main.py
+++ main.py
@@ -1,29 +1,12 @@
-from fastapi import FastAPI, Body
-from pydantic import BaseModel, Field
+from fastapi import FastAPI
 
+from api.polls import router
+
+
+#init the app   
 app = FastAPI()
 
-class UserProfile(BaseModel):
-    name: str
-    description: str
-    price: float = Field(gt = 10, description = "The price must be greater than 10")
-    tax: float | None = None
+#register the routes
+app.include_router(router, prefix="/polls")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-async def get_item(item_id: int):
-    return {"item_id": item_id}
-
-@app.get("/users/{user_id}/items/{item_id}")
-async def get_user_item(user_id: str, item_id: str):
-    return {
-        "user": user_id,
-        "item": item_id
-    }
-
-@app.post("/users/create_user")
-async def create_user(user: UserProfile = Body(embed=True)):
-    return {"name": user.name}
